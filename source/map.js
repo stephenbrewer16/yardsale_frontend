76776cac5b287fc37755cd816583717b0138fedf
Aaron Smith <smitham50@gmail.com>
2019-07-09 11:15:00 -0400

    added map

---
+++ source/map.js
@@ -0,0 +1,16 @@
+console.log("hi")
+
+// const mapDiv = document.querySelector('#map')
+
+// mapDiv.innerHTML = `<div id="leafletmap"></div>`
+
+const map = L.map('map').setView([51.505, -0.09], 13)
+
+const mapWindow = L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1Ijoic21pdGhhbTUwIiwiYSI6ImNqeHZ4NWNxcjA5cGYzY29jZjBlNnFub2kifQ.s8Am01fhZoezrzcGmFV1SQ', {
+    attribution: "Thank you, Mapbox!",
+    minZoom: 3,
+    maxZoom: 14,
+    id: 'mapbox.streets',
+    accessToken: 'pk.eyJ1Ijoic21pdGhhbTUwIiwiYSI6ImNqeHZ4NWNxcjA5cGYzY29jZjBlNnFub2kifQ.s8Am01fhZoezrzcGmFV1SQ'
+}).addTo(map)
+
